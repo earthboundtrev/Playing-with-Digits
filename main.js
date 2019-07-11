7ea6c01bf096289f63193804486bc7b023993a23
Trevor Thompson <Thompson_Trevor@bah.com>
2019-07-11 15:31:49 -0400

    Committing the code, so I don't lose the question because it is interesting

---
+++ main.js
@@ -0,0 +1,17 @@
+function digPow(n, p){
+
+let maxValue= 10;
+
+ for(let powerX=1; powerX < maxValue; powerX++)
+  {
+   
+      for(let powerY=1; powerY < maxValue; powerY++)
+         {
+	    
+	          if(Math.pow(n, powerX) + Math.pow(p, powerY) === )
+		     
+		        }
+			 }
+
+			   // ...
+			   }
